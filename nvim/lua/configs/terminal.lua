c5196ce8b09cf2498b21df5f03f9b9676444cafe
Kiyokatsu Ogushi <kohgushi@fastwave.gr.jp>
2025-05-06 22:59:54 +0900

    Updated Teminal mode behavior

--- nvim/lua/configs/terminal.lua
+++ nvim/lua/configs/terminal.lua
@@ -14,14 +14,20 @@ vim.opt.shellxquote = ''
 --
 
 -- Terminal at the bottom 
--- vim.keymap.set('n', 'tx', '<cmd>belowright new<CR><cmd>terminal<CR>', { noremap = true, silent = true })
--- vim.api.nvim_create_user_command('Tx', '<cmd>belowright new<CR><cmd>terminal<CR>', {})
-
+vim.keymap.set('n', 'bt', '<cmd>belowright new<CR><cmd>terminal<CR>', { noremap = true, silent = true })
 
 -- Insert mode when terminal launches
 local group_terminal = vim.api.nvim_create_augroup('terminal', { clear = true })
 vim.api.nvim_create_autocmd({ 'TermOpen' }, {
   pattern = '*',
-   group =  group_terminal,
+  group =  group_terminal,
   command = 'startinsert',
 })
+
+
+--
+-- Keymapping in Terminal mode
+--
+
+-- <esc><esc> => Terminal-normal mode
+vim.api.nvim_set_keymap('t', '<ESC><ESC>', '<C-\\><C-N>', { noremap = true, silent = true })
