8cfe08f4228dcf145b05cc273c700e25fdfe2da6
Kiyokatsu Ogushi <kohgushi@fastwave.gr.jp>
2025-05-06 23:00:46 +0900

    Supported live_grep in telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,6 +2,6 @@ return {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    lazy = true,  -- Lasy load doesn't work because key mapping below is needed to invoke Telescope
+    lazy = true,
   }
 }
