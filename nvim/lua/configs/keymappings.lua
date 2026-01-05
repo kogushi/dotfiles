4a03d8f60988fc1ed5c61dc1eb6a503a27e63fe5
Kiyokatsu Ogushi <kohgushi@fastwave.gr.jp>
2026-01-06 00:18:55 +0900

    Add quickhl-vim

--- nvim/lua/configs/keymappings.lua
+++ nvim/lua/configs/keymappings.lua
@@ -7,7 +7,7 @@ vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true
 -- Disable search highlight by ESCx2
 vim.api.nvim_set_keymap('n', '<ESC><ESC>', ':nohl<CR>', { noremap = true, silent = true })
 
--- Key mappting to invoke Telescope
+-- Key mapping to invoke Telescope
 local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
@@ -16,3 +16,9 @@ vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help 
 
 -- Invoke my help
 vim.keymap.set('n', '<leader>h', ':help myhelp.txt<CR>', { noremap = true, silent = true })
+
+-- Key Mapping to invoke quickhl-vim
+vim.keymap.set('n', '<leader>m', '<Plug>(quickhl-manual-this)', { silent = true })
+vim.keymap.set('x', '<leader>m', '<Plug>(quickhl-manual-this)', { silent = true })
+vim.keymap.set('n', '<leader>M', '<Plug>(quickhl-manual-reset)', { silent = true })
+vim.keymap.set('x', '<leader>M', '<Plug>(quickhl-manual-reset)', { silent = true })
